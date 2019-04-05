dcddd53d87bd1fa76c8db2dc8a8efc5a5c2faa1a
isaacms85 <48299325+isaacms85@users.noreply.github.com>
2019-04-05 11:50:22 -1000

    Add files via upload

---
+++ obstacle.js
@@ -0,0 +1,33 @@
+      function obstacle() {
+        this.x = w + 50;
+        this.y = h-140;
+        this.gravity = 0.5; //The force of gravity
+        this.velocity = 15; //Starting velocity of obstacle
+        
+        this.show = function() {
+          fill(255);
+          rect(this.x, this.y, 50, 100);
+          if (this.x < 0) {
+            this.velocity += 1;
+            this.x = w;
+            this.y = h-140;
+          }
+        };
+        
+        this.update = function() {
+          hit = collideRectRect(this.x+40, this.y, 50, 40, player.x+40, player.y-10, 50, 50);
+          this.x -= this.velocity;
+          this.velocity *= 1;
+          if (this.y > h) {
+            this.y = h;
+          }
+          if (this.y < 0) {
+            this.y = 0;
+          }
+          if (hit) {
+            window.alert('Oof');
+            noLoop();
+            location.reload();
+          }
+        };
+      }
---
+++ player.js
@@ -0,0 +1,43 @@
+      function player() {
+        this.x = 100;
+        this.y = h-90;
+        this.gravity = 1; //The force of gravity
+        this.lift = -50; //opposing the force of gravity
+        this.velocity = 0; //Starting velocity of player
+
+        this.show = function() {
+          fill(255,0,0);
+          rect(this.x, this.y, 50, 50);
+        };
+        
+        this.up = function() {
+          this.velocity += this.lift; //Jumping function
+          this.velocity += this.gravity;
+        };
+        
+        this.update = function() {
+          this.velocity += this.gravity; //Gravity applied when not jumping
+          this.y += this.velocity;
+          this.velocity *= 0.8;
+          if (this.y > h-90) {
+            this.y = h-90;
+            this.velocity = 0;
+          }
+          if (keyIsDown(37) && (this.x > 0)) {
+            this.x -= 5;
+          }
+          if (keyIsDown(39) && (this.x < w-25)) {
+            this.x += 5;
+          }
+          if ((keyIsDown(32)) && (this.y == h-90)) {
+            this.up();
+          }
+        };
+        
+        this.left = function() {
+          this.x -=5;
+        }
+        this.right = function() {
+          this.x += 5;
+        }
+      }
