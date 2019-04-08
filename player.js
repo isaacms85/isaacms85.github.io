149f0e72c1d6718b216ca8a2ae7a6bddaf2c93e4
isaacms85 <48299325+isaacms85@users.noreply.github.com>
2019-04-08 12:09:43 -1000

    Update player.js

--- player.js
+++ player.js
@@ -6,7 +6,7 @@
         this.velocity = 0; //Starting velocity of player
 
         this.show = function() {
-          image(imgP , this.x, this.y, 50, 50);
+          image(imgP , this.x, this.y, 50, 100);
         };
         
         this.up = function() {
