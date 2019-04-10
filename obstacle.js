cff7229a1705825f5b537cd53afbe5223d2b86bf
isaacms85 <48299325+isaacms85@users.noreply.github.com>
2019-04-10 11:30:48 -1000

    Update obstacle.js

--- obstacle.js
+++ obstacle.js
@@ -14,7 +14,7 @@
         };
         
         this.update = function() {
-          hit = collideRectRect(this.x+40, this.y, 50, 100, player.x+65, player.y, 75, 50);
+          hit = collideRectRect(this.x, this.y, 50, 100, player.x+65, player.y, 75, 50);
           this.x -= this.velocity;
           this.velocity *= 1;
           if (this.y > h) {
