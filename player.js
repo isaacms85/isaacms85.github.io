ae0c6c0630a2d3ecca06315f92f117c95f2759eb
isaacms85 <48299325+isaacms85@users.noreply.github.com>
2019-04-10 20:50:42 -1000

    Update player.js

--- player.js
+++ player.js
@@ -19,7 +19,7 @@
           this.y += this.velocity;
           this.velocity *= 0.8;
           if (this.y > h-90) {
-          this.y = h-100;
+          this.y = h-90;
           }
           if (keyIsDown(37) && (this.x > 0)) {
             this.x -= 5;
