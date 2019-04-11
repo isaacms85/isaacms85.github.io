6069829a8417f1f31407e39c18df4bd08e6e8675
isaacms85 <48299325+isaacms85@users.noreply.github.com>
2019-04-10 21:16:43 -1000

    Update obstacle.js

--- obstacle.js
+++ obstacle.js
@@ -5,7 +5,7 @@
         this.velocity = 15; //Starting velocity of obstacle
         
         this.show = function() {
-            image(imgU, this.x, this.y, 50, 200);
+            image(imgU, this.x, this.y, 58, 100);
           if (this.x < 0) {
             this.x = w+50;
             this.y = h-140;
@@ -13,7 +13,7 @@
         };
         
         this.update = function() {
-          hit = collideRectRect(this.x, this.y, 45, 100, player.x, player.y, 80, 50);
+          hit = collideRectRect(this.x, this.y, 53, 100, player.x, player.y, 80, 50);
           this.x -= this.velocity;
           this.velocity *= 1;
           if (this.y > h) {
